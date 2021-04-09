468438b8d9951680633aef662c87d64ea11db6e9
akhilvallala <55375393+akhilvallala@users.noreply.github.com>
2021-04-09 14:57:36 +0530

    Update Practice_code.py

--- Practice_code.py
+++ Practice_code.py
@@ -1,10 +1,27 @@
-#Fetching data from Azure Blob Storage.
+from pyspark.sql import *
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
+from pyspark.sql.functions  import date_format
+
+#Extreacting data from Azure Blob Storage.
              
 dbutils.fs.mount(
   source = "wasbs://<container-name>@<storage-account-name>.blob.core.windows.net",
   mount_point = "/mnt/<mount-name>",
   extra_configs = {"<conf-key>":dbutils.secrets.get(scope = "<scope-name>", key = "<key-name>")})
 
+#Tranformations,
+
+#date format
+def dateformat(df,format):
+  try:
+    for i in range(0,len(df.dtypes)):
+      if (df.dtypes[i][1] == 'date'):
+        column = df.dtypes[i][0]
+        df = df.withColumn(column,F.date_format(F.col(column),format) )
+    return df
+  except Exception as dateformaterror:
+    raise dateformaterror
 
 #Load into database
 jdbcHostname = "azsqlshackserver.database.windows.net"
